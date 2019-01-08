a6cfda55a589fa501d4ee0577edf74ee5e8e6d33
Patrick <bakospatrick@gmail.com>
2019-01-08 21:10:06 +0100

    INDEX.HTML index page listing question SERVER.PY index route modified to work properly UTIL.PY general sort function written for current project

---
+++ util.py
@@ -0,0 +1,11 @@
+import data_manager
+
+
+def sort_by_key(key='submission_time', reverse=False, database='questions'):
+    if database == 'questions':
+        database = data_manager.QUESTION_FILE_PATH
+    else:
+        database = data_manager.ANSWER_FILE_PATH
+    file = data_manager.read_from_csv(database)
+    file.sort(key=lambda row: row['submission_time'], reverse=reverse)
+    return file
