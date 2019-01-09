7789018ca1cfe05b6a27aad52a8086e72666cc5f
Patrick <bakospatrick@gmail.com>
2019-01-09 09:05:48 +0100

    SERVER.PY index route cleaned

--- server.py
+++ server.py
@@ -8,12 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    questions = []
-    messages = data_manager.read_from_csv()
     sorted_messages = util.sort_by_key()
-    return render_template('index.html',
-                           messages=messages,
-                           sorted_messages=sorted_messages)
+    return render_template('index.html', sorted_messages=sorted_messages)
 
 
 if __name__ == "__main__":
