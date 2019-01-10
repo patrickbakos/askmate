09e32a05774cb66ad28423440559621be9554aaf
Petrav <vagi.p94@gmail.com>
2019-01-10 14:08:30 +0100

    DATA_MANAGER.PY new functions: overwrite_old_csv and find_answer_id

--- data_manager.py
+++ data_manager.py
@@ -69,3 +69,41 @@ def collect_data(recieved_data, header=QUESTIONS_HEADER):
     return message
 
 
+def overwrite_old_csv(new_data, file=QUESTION_FILE_PATH):
+    """Can be used to overwrite the whole csv with the new data,
+        for example when you want to delete a row from the csv.
+
+        Args:
+        new_data(list of dictionaries): contains the new data
+        file(str): name of file to overwrite
+        """
+
+    if file == QUESTION_FILE_PATH:
+        header = QUESTIONS_HEADER
+    else:
+        header = ANSWERS_HEADER
+
+    with open(file, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=header)
+        writer.writeheader()
+
+        for row in new_data:
+            writer.writerow(row)
+
+
+def find_answer_id(question_id, file=ANSWER_FILE_PATH):
+    """Finds the IDs of the answers which belong to the given question
+
+        Args:
+        question_id(str): ID of the given question
+        file(str): name of file which contains the answers
+
+        Returns: list of strings"""
+
+    answer_ids = []
+    answer_reader = read_from_csv(file)
+
+    for row in answer_reader:
+        if row['question_id'] == question_id:
+            answer_ids.append(row['id'])
+    return answer_ids
